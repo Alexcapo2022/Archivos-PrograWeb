d15a965600c7d86710c49de08359212d7a7d480d
Alexander Cruz <20192659@aloe.ulima.edu.pe>
2022-09-30 02:45:00 -0500

    avance 30/09

---
+++ 20222_3-login-master/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './index.css';
+import LoginPage from './pages/LoginPage';
+import MainPage from './pages/MainPage';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <LoginPage /> } />
+        <Route path='/main' element={ <MainPage /> } />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ 20222_3-login-master/src/pages/LoginPage.jsx
@@ -0,0 +1,107 @@
+import React, { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+
+
+
+const LoginPage = () => {
+    // Variables de estado
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [carreraIdSelected, setCarreraIdSelected] = useState(-1)
+    const [error, setError] = useState(false)
+
+    // Hook navegacion
+    const navigate = useNavigate();
+
+    const httplogin = () => {
+        //Abrir un canal de comunicacion con un backend (servidor)
+        const prom = fetch("https://60b83e68b54b0a0017c03380.mockapi.io/users")
+        prom.then((resp)=>{
+            //Tarea deada la respuesta del servidor
+            return resp.json()
+        }).then((data)=>{
+            //data ya es un objeto javascript
+            setUsername(data[0].username)
+        })
+    
+    }
+
+    const loginOnClick = () => {
+        httplogin();
+        /*if (username !== "" && password !== "" && carreraIdSelected !== -1) {
+            // Si puedo hacer el login
+            navigate("/main")
+        }else {
+            // Error
+            setError(true)
+        }*/
+    }
+
+    return <div className="container">
+        <h1>Login</h1>
+        <div>
+            <label className="form-label">
+                Usuario
+            </label>
+            <input className="form-control"
+                value={ username }
+                onChange={ (evt) => setUsername(evt.target.value) } />
+        </div>
+        <div>
+            <label className="form-label">
+                Password
+            </label>
+            <input type="password" 
+                className="form-control"
+                value={ password }
+                onChange={ (evt) => setPassword(evt.target.value) } />
+        </div>
+        <div>
+            <label className="form-label">
+                Carrera
+            </label>
+            <select className="form-select"
+                value={ carreraIdSelected }
+                onChange={ (evt) => setCarreraIdSelected(evt.target.value) }>
+                <option value={ -1 }>--- Ingrese su carrera ---</option>
+                <option value={ 1 }>Ingeniería de Sistemas</option>
+                <option value={ 2 }>Ingeniería Industrial</option>
+                <option value={ 3 }>Ingeniería Civil</option>
+            </select>
+        </div>
+        <button className="btn btn-primary" 
+            type="button"
+            onClick={ loginOnClick } >
+                Login
+        </button>
+        <div>
+            {
+                (() => {
+                    // Validacion de formularios
+
+                    const arrAlerts = []
+                    if (error && username === ""){
+                        arrAlerts.push(<div className="alert alert-danger">
+                            Ingrese Username
+                        </div>)
+                    }
+                    if (error && password === ""){
+                        arrAlerts.push(<div className="alert alert-danger">
+                            Ingrese Password
+                        </div>)
+                    }
+                    if (error && carreraIdSelected === -1){
+                        arrAlerts.push(<div className="alert alert-danger">
+                            Seleccione carrera
+                        </div>)
+                    }
+                    return arrAlerts
+
+                })()
+            }
+            
+        </div>
+    </div>
+}
+
+export default LoginPage
