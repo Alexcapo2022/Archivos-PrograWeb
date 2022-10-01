5819f20f1d7c1fc9883ae59fe609ef0fc6fcb68d
Alexander Cruz <20192659@aloe.ulima.edu.pe>
2022-10-01 04:27:07 -0500

    avance 1/10

--- Proyecto del Curso/Build Begginers/INTERFACES/js/scripts.js
+++ Proyecto del Curso/Build Begginers/INTERFACES/js/scripts.js
@@ -66,6 +66,35 @@ function renderCarrito(){
             <td class="table__price"><p>${item.precio}</p></td>
             <td class="table__cantidad">
               <input type="number" min="1" value=${item.cantidad} class="input__elemento">
+              <button class="delete btn btn-danger"><img src="/media/basura.png" style="height:30px ;width:32px"</button>
+            </td>
+    
+    `
+    tr.innerHTML = Content;
+    tbody.append(tr)
+
+    tr.querySelector(".delete").addEventListener('click', removeItemCarrito)
+    tr.querySelector(".input__elemento").addEventListener('change', sumaCantidad)
+  })
+  CarritoTotal()
+}
+
+/*
+function renderCarrito(){
+  tbody.innerHTML = ''
+  carrito.map(item => {
+    const tr = document.createElement('tr')
+    tr.classList.add('ItemCarrito')
+    const Content = `
+    
+    <th scope="row">1</th>
+            <td class="table__productos">
+              <img src=${item.img}  alt="">
+              <h6 class="title">${item.title}</h6>
+            </td>
+            <td class="table__price"><p>${item.precio}</p></td>
+            <td class="table__cantidad">
+              <input type="number" min="1" value=${item.cantidad} class="input__elemento">
               <button class="delete btn btn-danger">x</button>
             </td>
     
@@ -78,6 +107,7 @@ function renderCarrito(){
   })
   CarritoTotal()
 }
+*/
 
 function CarritoTotal(){
   let Total = 0;
